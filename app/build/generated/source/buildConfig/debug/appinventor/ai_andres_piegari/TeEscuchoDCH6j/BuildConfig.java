a2469fd726a61b65b9722d52f4b8bf44ce978a48
marouxet <marianoarouxet@gmail.com>
2021-03-04 13:58:52 -0300

    Version 2.0 - Subida a Google Play

---
+++ app/build/generated/source/buildConfig/debug/appinventor/ai_andres_piegari/TeEscuchoDCH6j/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package appinventor.ai_andres_piegari.TeEscuchoDCH6j;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "appinventor.ai_andres_piegari.TeEscuchoDCH6j";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "2.0";
+}
